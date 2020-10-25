8b689df71fdae4240fe824b9072d15808cb07522
khandu1 <aargade123@gmail.com>
2020-10-25 18:35:00 +0530

    Update index.php
    just added few line to cofirm. !! sorry for that

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "Hello World!";
+echo "Hello World! This is site running on azure webapps and if you are reading this means, You have created azure web app and you are awasome (@_|_@)";
